729e9fb21a64b2cd4de57e523fdc2713593ba3a5
William Chong <wchong@redurban.ca>
2014-03-12 21:16:25 -0400

    figured out the video spot

--- js/script.js
+++ js/script.js
@@ -16,6 +16,30 @@ $( document ).ready(function() {
       mobileNavOpen = false;
     }
   });
+  
+    var theWidth = $('.main-video').width();
+    var theHeight = theWidth*.6304;
+    $('.main-video').css({'height': theHeight+"px"});
+    $('.main-video').css({'background-size':theWidth});
+    //var newTop = (theHeight - (theHeight*.7326))*.7326;
+    $('.main-video .thevideo').css({'width': theWidth*.739+"px",'height':theHeight/546*400+"px"});
+    var theTop = ($('.main-video').height()-$('.main-video .thevideo').height())*(66/146);
+    $('.main-video .thevideo').css({'top': theTop+"px" });
+
+  
+  $(window).resize(function(){
     
+    var theWidth = $('.main-video').width();
+    var theHeight = theWidth*.6304;
+    $('.main-video').css({'height': theHeight+"px"});
+    $('.main-video').css({'background-size':theWidth});
+    //var newTop = (theHeight - (theHeight*.7326))*.7326;
+    $('.main-video .thevideo').css({'width': theWidth*.739+"px",'height':theHeight/546*400+"px"});
+    //console.log($('.main-video .thevideo').height()/$('.main-video').height());
+    //console.log();
+    
+    var theTop = ($('.main-video').height()-$('.main-video .thevideo').height())*(66/146);
+    $('.main-video .thevideo').css({'top': theTop+"px" });
+  });
     
 });
